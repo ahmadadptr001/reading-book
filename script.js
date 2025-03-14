482117b05d50adc095a72802a14adfd18ea6d190
ahmadadptr001 <ahmadadptr@gmail.com>
2025-03-14 07:00:24 +0700

    penambahan sweet alert dan animasi transisi aos

---
+++ script.js
@@ -0,0 +1,11 @@
+function alertSupport() {
+      Swal.fire({
+            title: "Sukses !",
+            text: "Thanks For Support",
+            icon: "success",
+            confirmButtonColor: "#3085d6",
+            confirmButtonText: "OK",
+      }).then((result) => {
+            window.open("https://youtube.com/ahmadadptr", "_blank");
+      });
+}
